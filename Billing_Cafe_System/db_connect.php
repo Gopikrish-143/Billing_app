2dc474b316dd845a2643aee5f5b13287b7d35352
Gopikrishnan Ethiraj <126351148+Gopikrish-143@users.noreply.github.com>
2025-03-11 13:08:32 +0530

    Update db_connect.php

--- Billing_Cafe_System/db_connect.php
+++ Billing_Cafe_System/db_connect.php
@@ -1,3 +1,12 @@
-<?php 
+<?php
+$host = "sql308.infinityfree.com"; // Check your InfinityFree MySQL Hostname
+$user = "if0_38493441"; // Your InfinityFree username
+$pass = "your_password"; // Your InfinityFree database password
+$db = "if0_38493441_cafe_billing_db"; // Your InfinityFree database name
 
-$conn= new mysqli('localhost','root','','cafe_billing_db')or die("Could not connect to mysql".mysqli_error($con));
+$conn = mysqli_connect($host, $user, $pass, $db);
+
+if(!$conn){
+    die("Connection Failed: " . mysqli_connect_error());
+}
+?>

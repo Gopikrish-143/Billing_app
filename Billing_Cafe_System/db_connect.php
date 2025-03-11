d067db2644fa16228c56f8ead32c92c436969620
Gopikrishnan Ethiraj <126351148+Gopikrish-143@users.noreply.github.com>
2025-03-11 13:13:33 +0530

    Update db_connect.php
    db Password changed

--- Billing_Cafe_System/db_connect.php
+++ Billing_Cafe_System/db_connect.php
@@ -1,7 +1,7 @@
 <?php
 $host = "sql308.infinityfree.com"; // Check your InfinityFree MySQL Hostname
 $user = "if0_38493441"; // Your InfinityFree username
-$pass = "your_password"; // Your InfinityFree database password
+$pass = "Gopikrish@143"; // Your InfinityFree database password
 $db = "if0_38493441_cafe_billing_db"; // Your InfinityFree database name
 
 $conn = mysqli_connect($host, $user, $pass, $db);
